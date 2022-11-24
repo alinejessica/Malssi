29985fdb0c9f95bf548926db52afbc00be4a6362
alunoctg <alunoctg@fiern.local>
2022-11-24 07:36:02 -0300

    first commit

---
+++ agendasalao/myapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('altersenha/', views.altersenha, name='altersenha'),
+    path('errocadastro/', views.errocadastro, name='errocadastro'),
+    path('erroemail/', views.erroemail, name='erroemail'),
+    path('erroLogin/', views.erroLogin, name='erroLogin'),
+    path('errouser/', views.errouser, name='errouser'),
+    path('esqueceusenha/', views.esqueceusenha, name='esqueceusenha'),
+    path('produtos/', views.produtos, name='produtos'),
+    path('lancamentos/', views.lancamentos, name='lancamentos'),
+    path('inicio/', views.inicio, name='inicio'),
+]
---
+++ agendasalao/myapp/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    data = {}
+    return render(request, 'index.html', data)
+
+def home(request):
+    data = {}
+    return render(request, 'home.html', data)
+
+def login(request):
+    data = {}
+    return render(request, 'Loginprincipal.html', data)
+
+def cadastro(request):
+    data = {}
+    return render(request, 'CADASTRO.html', data)
+
+def altersenha(request):
+    data = {}
+    return render(request, 'altersenha.html', data)
+
+def salvarsenha(request):
+    data = {}
+    return render(request, 'altersenha.html', data)
+    
+def errocadastro(request):
+    data = {}
+    return render(request, 'errocadastro.html', data)
+    
+def erroemail(request):
+    data = {}
+    return render(request, 'erroemail.html', data)
+
+def erroLogin(request):
+    data = {}
+    return render(request, 'erroLogin.html', data)
+    
+def errouser(request):
+    data = {}
+    return render(request, 'errouser.html', data)
+
+def esqueceusenha(request):
+    data = {}
+    return render(request, 'esqueceusenha.html', data)
+
+def produtos(request):
+    data = {}
+    return render(request, 'HISTORIA.html', data)
+
+def lancamentos(request):
+    data = {}
+    return render(request, 'LANÇAMENTOS.html', data)
+
+def logout(request):
+    data = {}
+    return render(request, 'logout.html', data)
+
+def profile(request):
+    data = {}
+    return render(request, 'profile.html', data)
+
+def suceslogin(request):
+    data = {}
+    return render(request, 'suceslogin.html', data)
+    
+def inicio(request):
+    data = {}
+    return render(request, 'INDES.html', data)
