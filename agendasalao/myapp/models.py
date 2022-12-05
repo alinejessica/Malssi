565efa4840bd55e90350f8d51c1f4c724fa2775f
alinejessica <113519076+alinejessica@users.noreply.github.com>
2022-12-05 13:59:03 -0300

    Update models.py

--- agendasalao/myapp/models.py
+++ agendasalao/myapp/models.py
@@ -4,7 +4,7 @@ class Funcionario(models.Model) :
     cpf = models.CharField (max_length=11)
     procedimento = models.CharField(max_length=20)
     ativo = models.BooleanField(max_length= 1) 
-    gerente = models.BooleanField
+    gerente = models.BooleanField(max_length=1)
 
 
 
@@ -13,15 +13,15 @@ class Cadastro (models.Model):
     sobrenome = models.CharField(max_length=50) 
     email = models.CharField(max_length=30) 
     senha = models.CharField(max_length=10) 
-    telefone =  models.IntegerField(11) 
-    data_de_nasc = models.IntegerField()
+    telefone =  models.IntegerField(max_length=11) 
+    data_de_nasc = models.IntegerField(max_length=8)
     endereco = models.CharField(max_length=30)
 
 
 
 class Agendamento (models.Model):
-    data_agendamento = models.IntegerField()
-    horário_agendamento = models.IntegerField()
+    data_agendamento = models.IntegerField(max_length=6)
+    horário_agendamento = models.IntegerField(max_length=4)
     funcionario = models.ForeignKey(Funcionario,on_delete=models.CASCADE)
     cadastro= models.ForeignKey(Cadastro,on_delete=models.CASCADE)
 
@@ -29,22 +29,22 @@ class Agendamento (models.Model):
 
 class Endereco (models.Model):
     rua  = models.CharField(max_length=40)
-    número  = models.IntegerField(4) 
-    cidade =  models.CharField()
-    cep = models.IntegerField(8)
+    número  = models.IntegerField(max_length=40) 
+    cidade =  models.CharField(max_length=40)
+    cep = models.IntegerField(max_length=11)
 
 
 
 class Procedimento(models.Model):
-    descricao = models.CharField(255) 
-    data_proced = models.IntegerField()
-    tempo_proced  = models.IntegerField()
+    descricao = models.CharField(max_length=40) 
+    data_proced = models.IntegerField(max_length=4)
+    tempo_proced  = models.IntegerField(max_length=4)
     valor = models.CharField(max_length=5)
 
 
 
 class Salao(models.Model):
-    nome = models.CharField(15) 
+    nome = models.CharField(max_length=45) 
     endereco = models.CharField(max_length=30)
-    funcionario  = models.IntegerField()
+    funcionario  = models.IntegerField(max_length=5)
     ativo = models.BooleanField(max_length= 1)
